9b8d4983cf1302fd268b0750ea9ddc863ae0c266
Michael Shingo Crawford <mcrawford5376@gmail.com>
2025-04-20 13:22:14 +0900

    fix product params

--- src/app/api/stripe/prices/[productId]/route.ts
+++ src/app/api/stripe/prices/[productId]/route.ts
@@ -1,10 +1,9 @@
 import { stripe } from '@/stripe/client';
 import { NextResponse } from 'next/server';
 
-export async function GET(
-	request: Request,
-	{ params }: { params: { productId: string } }
-) {
+type Params = Promise<{ productId: string }>;
+
+export async function GET(request: Request, { params }: { params: Params }) {
 	const { productId } = await params;
 	try {
 		const prices = await stripe.prices.list({
