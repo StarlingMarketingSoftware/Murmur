e2257f73fa380b87f4db2b7f9adee0a9c84836c6
Michael Shingo Crawford <mcrawford5376@gmail.com>
2025-04-26 09:58:40 +0900

    fix typscript errors

--- src/components/organisms/dialogs/ManageContactListDialog/ManageContactListDialog.tsx
+++ src/components/organisms/dialogs/ManageContactListDialog/ManageContactListDialog.tsx
@@ -2,7 +2,6 @@ import { Button } from '@/components/ui/button';
 import {
 	Dialog,
 	DialogContent,
-	DialogDescription,
 	DialogFooter,
 	DialogHeader,
 	DialogTitle,
@@ -22,7 +21,6 @@ export const ManageContactListDialog: FC<ManageContactListDialogProps> = (props)
 		isOpen,
 		setIsOpen,
 		columns,
-		setSelectedRows,
 		selectedContactList,
 		saveSelectedRecipients,
 	} = useManageContactListDialog(props);
