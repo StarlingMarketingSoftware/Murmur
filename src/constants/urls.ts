dd10a2471c34cd733f6a90d06f32d32d0579b464
Michael Shingo Crawford <mcrawford5376@gmail.com>
2025-04-26 09:51:21 +0900

    admin home

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -17,6 +17,19 @@ export const urls = {
 			path: '/murmur/campaign',
 		},
 	},
+	admin: {
+		path: '/admin',
+		label: 'Admin',
+		category: 'protected',
+		contacts: {
+			path: '/admin/contacts',
+			label: 'Contacts',
+		},
+		products: {
+			path: '/admin/products',
+			label: 'Products',
+		},
+	},
 	pricing: {
 		path: '/pricing',
 		label: 'Pricing',
