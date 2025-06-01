314142b3d795854b83c044c2887d9975fb2bb6b9
Michael Shingo Crawford <mcrawford5376@gmail.com>
2025-06-01 12:27:49 +0900

    fix build

--- src/components/organisms/_dialogs/ConfirmSendDialog/useConfirmSendDialog.tsx
+++ src/components/organisms/_dialogs/ConfirmSendDialog/useConfirmSendDialog.tsx
@@ -87,7 +87,7 @@ export const useConfirmSendDialog = (props: ConfirmSendDialogProps) => {
 				recipientEmail: email.contact.email,
 				senderEmail: form.getValues().senderEmail,
 				senderName: form.getValues().senderName,
-				userMurmurEmail: user.murmurEmail,
+				userMurmurEmail: user?.murmurEmail,
 			});
 			if (res.success) {
 				await updateEmail({
