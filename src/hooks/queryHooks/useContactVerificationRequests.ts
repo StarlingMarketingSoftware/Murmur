1b2d73f0d4d9a0be9c795c00e33359002b21fc49
Michael Shingo Crawford <mcrawford5376@gmail.com>
2025-06-05 21:17:23 +0900

    frontend

--- src/hooks/queryHooks/useContactVerificationRequests.ts
+++ src/hooks/queryHooks/useContactVerificationRequests.ts
@@ -2,11 +2,13 @@ import { ContactVerificationRequest } from '@prisma/client';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { CustomMutationOptions } from '@/types';
 import { toast } from 'sonner';
-import { PostContactData } from '@/app/api/contacts/route';
 import { appendQueryParamsToUrl } from '@/utils';
 import { _fetch } from '@/utils';
 import { urls } from '@/constants/urls';
-import { PatchContactVerificationRequestData } from '@/app/api/contact-verification-requests/route';
+import {
+	PatchContactVerificationRequestData,
+	PostContactVerificationRequestData,
+} from '@/app/api/contact-verification-requests/route';
 
 const QUERY_KEYS = {
 	all: ['contactVerficationRequest'] as const,
@@ -36,15 +38,15 @@ export const useCreateContactVerificationRequest = (
 ) => {
 	const {
 		suppressToasts = false,
-		successMessage = 'Contact created successfully',
-		errorMessage = 'Failed to create contact',
+		successMessage = 'Contacts successfully sent for verification',
+		errorMessage = 'Failed to sent contacts for verification',
 		onSuccess: onSuccessCallback,
 	} = options;
 
 	const queryClient = useQueryClient();
 
 	return useMutation({
-		mutationFn: async (data: PostContactData) => {
+		mutationFn: async (data: PostContactVerificationRequestData) => {
 			const response = await _fetch(
 				urls.api.contactVerificationRequests.index,
 				'POST',
@@ -89,60 +91,19 @@ export const useCheckContactVerificationRequest = (
 				'PATCH',
 				data
 			);
+			console.log('🚀 ~ mutationFn: ~ response:', response);
+			console.log(response.ok);
 			if (!response.ok) {
 				const errorData = await response.json();
-				throw new Error(
-					errorData.error || 'Failed to update contact verification request'
-				);
-			}
-
-			return response.json();
-		},
-		onSuccess: (variables) => {
-			queryClient.invalidateQueries({ queryKey: QUERY_KEYS.list() });
-			queryClient.invalidateQueries({
-				queryKey: QUERY_KEYS.detail(variables.id),
-			});
-
-			if (!suppressToasts) {
-				toast.success(successMessage);
-			}
-			onSuccessCallback?.();
-		},
-		onError: () => {
-			if (!suppressToasts) {
-				toast.error(errorMessage);
-			}
-		},
-	});
-};
-
-export const useVerifyContacts = (options: CustomMutationOptions = {}) => {
-	const {
-		suppressToasts = false,
-		successMessage = 'Contacts successfully sent for verification',
-		errorMessage = 'Failed to sent contacts for verification',
-		onSuccess: onSuccessCallback,
-	} = options;
-
-	const queryClient = useQueryClient();
-
-	return useMutation({
-		mutationFn: async (data: PostContactData) => {
-			const response = await _fetch(
-				urls.api.contactVerificationRequests.index,
-				'POST',
-				data
-			);
-			if (!response.ok) {
-				const errorData = await response.json();
-				throw new Error(errorData.error || 'Failed to create contact');
+				console.log('not ok');
+				throw new Error(errorData.error);
 			}
 
 			return response.json();
 		},
 		onSuccess: () => {
 			queryClient.invalidateQueries({ queryKey: QUERY_KEYS.list() });
+
 			if (!suppressToasts) {
 				toast.success(successMessage);
 			}
