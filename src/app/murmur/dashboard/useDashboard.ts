0fd6ee73baab458cd800b39ad3276d7e600671c3
Michael Shingo Crawford <mcrawford5376@gmail.com>
2025-06-29 15:15:34 +0900

    fix seed

--- src/app/murmur/dashboard/useDashboard.ts
+++ src/app/murmur/dashboard/useDashboard.ts
@@ -3,7 +3,7 @@
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import { useGetApollo } from '@/hooks/queryHooks/useApollo';
+// import { useGetApollo } from '@/hooks/queryHooks/useApollo';
 import { useCreateContactList } from '@/hooks/queryHooks/useContactLists';
 import { ContactList, EmailVerificationStatus } from '@prisma/client';
 import { useState } from 'react';
@@ -44,18 +44,18 @@ export const useDashboard = () => {
 		enabled: false,
 	});
 
-	const {
-		data: apolloContacts,
-		isPending: isPendingApolloContacts,
-		isLoading: isLoadingApolloContacts,
-		error: apolloError,
-		refetch: apolloRefetch,
-	} = useGetApollo({
-		filters: {
-			query: searchText,
-			limit: 5,
-		},
-	});
+	// const {
+	// 	data: apolloContacts,
+	// 	isPending: isPendingApolloContacts,
+	// 	isLoading: isLoadingApolloContacts,
+	// 	error: apolloError,
+	// 	refetch: apolloRefetch,
+	// } = useGetApollo({
+	// 	filters: {
+	// 		query: searchText,
+	// 		limit: 5,
+	// 	},
+	// });
 
 	const { mutate: createContactList } = useCreateContactList({
 		suppressToasts: true,
