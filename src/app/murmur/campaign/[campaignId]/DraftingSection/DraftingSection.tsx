3a5b5affe4847dbdf6cc7db56dcc6c4dcf3b85fb
Starlingmarketing <starlingmarketingagency@gmail.com>
2025-08-26 11:02:33 -0400

    can deploy

--- src/app/murmur/campaign/[campaignId]/DraftingSection/DraftingSection.tsx
+++ src/app/murmur/campaign/[campaignId]/DraftingSection/DraftingSection.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import { DraftingSectionProps, useDraftingSection } from './useDraftingSection';
-import { Button } from '@/components/ui/button';
 import {
 	FormField,
 	FormItem,
@@ -15,7 +14,6 @@ import { Separator } from '@/components/ui/separator';
 import { HybridPromptInput } from '@/components/molecules/HybridPromptInput/HybridPromptInput';
 import { UpgradeSubscriptionDrawer } from '@/components/atoms/UpgradeSubscriptionDrawer/UpgradeSubscriptionDrawer';
 import { DraftingMode } from '@prisma/client';
-import { cn } from '@/utils';
 import { EmailGeneration } from './EmailGeneration/EmailGeneration';
 
 export const DraftingSection: FC<DraftingSectionProps> = (props) => {
