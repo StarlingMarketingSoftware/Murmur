a2b1ae921ad2ae87edc273b84bdbdcaebe551f06
Michael Shingo Crawford <mcrawford5376@gmail.com>
2025-09-03 21:40:34 +0900

    filter out basic monthly

--- src/components/organisms/ProductList/ProductList.tsx
+++ src/components/organisms/ProductList/ProductList.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { ProductListProps, useProductList } from './useProductList';
 import { ProductCard } from '../ProductCard/ProductCard';
 import { Spinner } from '@/components/atoms/Spinner/Spinner';
+import { cn } from '@/utils';
 
 export const ProductList: FC<ProductListProps> = (props) => {
 	const { sortedProducts, user, isPendingProducts, billingCycle } = useProductList(props);
@@ -15,7 +16,12 @@ export const ProductList: FC<ProductListProps> = (props) => {
 	}
 
 	return (
-		<div className="grid grid-cols-1 lg:grid-cols-3 md:gap-5 lg:gap-5 xl:gap-28 w-fit mx-auto place-items-center [&>*:hover~*]:opacity-50 [&>*:hover~*]:scale-[0.99]">
+		<div
+			className={cn(
+				'grid grid-cols-1 md:gap-5 lg:gap-5 xl:gap-28 w-fit mx-auto place-items-center [&>*:hover~*]:opacity-50 [&>*:hover~*]:scale-[0.99]',
+				billingCycle === 'month' ? 'lg:grid-cols-2' : 'lg:grid-cols-3'
+			)}
+		>
 			{sortedProducts.map((product) => (
 				<div
 					key={product.id}
