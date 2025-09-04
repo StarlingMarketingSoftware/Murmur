7d103ed47910402d68e3e98c5dd13d90f4630123
Starlingmarketing <starlingmarketingagency@gmail.com>
2025-09-04 18:40:49 -0400

    Test prompt box bottom alignment

--- src/components/molecules/TestPreviewPanel/TestPreviewPanel.tsx
+++ src/components/molecules/TestPreviewPanel/TestPreviewPanel.tsx
@@ -241,8 +241,8 @@ export const TestPreviewPanel: FC<TestPreviewPanelProps> = ({
 
 	return (
 		<div className="w-1/2 flex flex-col">
-			<div className="flex-1 flex flex-col p-3">
-				<div className="flex-1 border-2 border-black rounded-lg bg-background flex flex-col overflow-hidden mb-[13px] mt-[40px]">
+			<div className="flex-1 flex flex-col px-3 pt-3 pb-0">
+				<div className="flex-1 border-2 border-black rounded-lg bg-background flex flex-col overflow-hidden mb-4 mt-[40px]">
 					<div className="relative p-4">
 						<Typography
 							variant="h3"
