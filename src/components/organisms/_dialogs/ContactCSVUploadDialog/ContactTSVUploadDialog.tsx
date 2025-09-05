0d98eda2fe85790e3488feb84290d9fe33f44ad9
Starlingmarketing <starlingmarketingagency@gmail.com>
2025-09-05 12:41:38 -0400

    upload contacts

--- src/components/organisms/_dialogs/ContactCSVUploadDialog/ContactTSVUploadDialog.tsx
+++ src/components/organisms/_dialogs/ContactCSVUploadDialog/ContactTSVUploadDialog.tsx
@@ -10,7 +10,6 @@ import {
 	ContactTSVUploadDialogProps,
 	useContactTSVUploadDialog,
 } from './useContactTSVUploadDialog';
-import { DownloadIcon, UploadIcon } from 'lucide-react';
 import CustomTable from '@/components/molecules/CustomTable/CustomTable';
 import { FC } from 'react';
 import { DialogDescription } from '@radix-ui/react-dialog';
@@ -52,59 +51,77 @@ export const ContactTSVUploadDialog: FC<ContactTSVUploadDialogProps> = (props) =
 				fullScreen={!!fullScreen}
 			>
 				<div className={cn('pt-8', fullScreen && 'w-full max-w-[1200px] mx-auto')}>
-					<DialogDescription className="text-center mt-2">
+					<DialogDescription className="text-center mt-2 invisible" aria-hidden>
 						Download the template below and open it in your preferred spreadsheet
 						software. Enter your data following the format of the file, then upload your
 						file.
 					</DialogDescription>
 					<div
 						className={cn(
-							'flex flex-row gap-4 justify-center items-center',
-							fullScreen && 'px-6 pt-2'
+							'mx-auto rounded-[8px] border-2 border-[#8C8C8C] flex flex-col relative overflow-hidden',
+							fullScreen && 'max-h-[calc(100vh-180px)] min-h-[420px]'
 						)}
 					>
-						<input
-							type="file"
-							ref={fileInputRef}
-							className="hidden"
-							accept=".tsv"
-							onChange={handleFileUpload}
+						<CustomTable
+							columns={columns}
+							data={tsvData}
+							noDataMessage="Upload a TSV file to load data."
+							useAutoLayout
+							allowColumnOverflow
+							searchable={false}
+							displayRowsPerPage={false}
+							hidePagination
+							variant="secondary"
+							containerClassName={cn('flex-1 min-h-0 pb-[64px] !border-0 !rounded-none')}
+							tableClassName="!justify-center"
+							headerClassName="!border-0 !text-[12px] [&_tr]:!border-0"
+							theadCellClassName="!border-0 !text-[12px] !border-b-0"
+							rowClassName="!border-0 [&_td]:!border-0"
 						/>
-						<Button onClick={handleUploadClick}>
-							<UploadIcon />
-							Upload TSV
-						</Button>
-						<Button variant="primary-light" onClick={handleTemplateDownload}>
-							<DownloadIcon />
-							Download TSV Template
-						</Button>
+						<div className="absolute bottom-0 left-0 right-0 bg-white px-6 py-3 flex items-center justify-end gap-4">
+							<input
+								type="file"
+								ref={fileInputRef}
+								className="hidden"
+								accept=".tsv"
+								onChange={handleFileUpload}
+							/>
+							<Button
+								variant="primary-light"
+								onClick={handleTemplateDownload}
+								style={{ fontFamily: 'Times New Roman', fontWeight: 700 }}
+								className="!bg-white !text-black !border !border-[#000000] !rounded-[8px]"
+							>
+								Download TSV Template
+							</Button>
+							<Button
+								onClick={handleUploadClick}
+								style={{ fontFamily: 'Times New Roman', fontWeight: 700 }}
+								className="!bg-[#5DAB68] !text-white !w-[174px] !h-[39px]"
+							>
+								Upload TSV
+							</Button>
+							<Button
+								onClick={handleClear}
+								type="button"
+								variant="light"
+								style={{ fontFamily: 'Times New Roman', fontWeight: 700 }}
+								className="!bg-white !text-black !border !border-[#000000] !rounded-[8px] !w-[174px] !h-[39px]"
+							>
+								Clear
+							</Button>
+						</div>
 					</div>
-					<CustomTable
-						columns={columns}
-						data={tsvData}
-						noDataMessage="Upload a TSV file to load data."
-						useAutoLayout
-						allowColumnOverflow
-						searchable={false}
-						displayRowsPerPage={false}
-						hidePagination
-						variant="secondary"
-						containerClassName={cn(
-							fullScreen && 'max-h-[calc(100vh-180px)] min-h-[420px]',
-							'mx-auto rounded-[8px] [&]:border-2 [&]:border-[#8C8C8C] [&]:!border-black-0'
-						)}
-						tableClassName="!justify-center"
-						headerClassName="!border-0 !text-[12px] [&_tr]:!border-0"
-						theadCellClassName="!border-0 !text-[12px] !border-b-0"
-						rowClassName="!border-0 [&_td]:!border-0"
-					/>
 					<DialogFooter
 						className={cn('justify-center items-center !border-0', fullScreen && 'pb-6')}
 					>
-						<Button onClick={handleClear} type="button" variant="light">
-							Clear
-						</Button>
-						<Button onClick={handleSave} isLoading={isPending} type="submit">
+						<Button
+							onClick={handleSave}
+							isLoading={isPending}
+							type="submit"
+							style={{ fontFamily: 'Times New Roman', fontWeight: 700 }}
+							className="!bg-white !text-black !border !border-[#5DAB68] !w-full"
+						>
 							Save
 						</Button>
 					</DialogFooter>
