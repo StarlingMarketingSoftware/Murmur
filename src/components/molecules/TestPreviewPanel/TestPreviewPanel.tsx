e25498773243f1d6709b53962ef2425bbb5e3049
Starlingmarketing <starlingmarketingagency@gmail.com>
2025-09-05 19:14:10 -0400

    test prompt alignment

--- src/components/molecules/TestPreviewPanel/TestPreviewPanel.tsx
+++ src/components/molecules/TestPreviewPanel/TestPreviewPanel.tsx
@@ -277,7 +277,7 @@ export const TestPreviewPanel: FC<TestPreviewPanelProps> = ({
 	return (
 		<div className="w-1/2 flex flex-col">
 			<div className="flex-1 flex flex-col px-3 pt-[18px] pb-0">
-				<div className="flex-1 border-2 border-black rounded-lg bg-background flex flex-col overflow-hidden mb-4 mt-[-4px]">
+				<div className="flex-1 border-2 border-black rounded-lg bg-background flex flex-col overflow-hidden mb-6 mt-[-4px]">
 					<div className="relative p-4">
 						<Typography
 							variant="h3"
