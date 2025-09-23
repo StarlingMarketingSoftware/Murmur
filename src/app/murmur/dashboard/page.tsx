1f5e8a93ea8c270c65b14064a6204422428f0799
Starlingmarketing <starlingmarketingagency@gmail.com>
2025-09-23 18:31:51 -0400

    search button added

--- src/app/murmur/dashboard/page.tsx
+++ src/app/murmur/dashboard/page.tsx
@@ -253,6 +253,35 @@ const Dashboard = () => {
 																			“Music Venues in North Carolina”
 																		</span>
 																	</div>
+																	{/* Mobile-only submit icon inside input */}
+																	<button
+																		type="submit"
+																		className="search-input-icon-btn"
+																		aria-label="Search"
+																	>
+																		<svg
+																			width="17"
+																			height="16"
+																			viewBox="0 0 17 16"
+																			fill="none"
+																			xmlns="http://www.w3.org/2000/svg"
+																			aria-hidden="true"
+																		>
+																			<path
+																				d="M9.82227 0.848633C12.8952 0.848855 15.2988 3.17275 15.2988 5.93457C15.2988 8.69637 12.8952 11.0203 9.82227 11.0205C6.74914 11.0205 4.34475 8.69651 4.34473 5.93457C4.34473 3.17261 6.74912 0.848633 9.82227 0.848633Z"
+																				stroke="#A0A0A0"
+																				strokeWidth="1.56483"
+																			/>
+																			<line
+																				x1="6.50289"
+																				y1="9.18905"
+																				x2="1.02598"
+																				y2="15.4484"
+																				stroke="#A0A0A0"
+																				strokeWidth="1.56483"
+																			/>
+																		</svg>
+																	</button>
 																</div>
 															</div>
 														</FormControl>
