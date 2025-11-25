0161d11fdc6426eb1b0d0499296d314e38b780c4
Starlingmarketing <starlingmarketingagency@gmail.com>
2025-11-24 23:09:12 -0500

    removed x button

--- src/app/murmur/dashboard/page.tsx
+++ src/app/murmur/dashboard/page.tsx
@@ -1618,22 +1618,6 @@ const Dashboard = () => {
 																</div>
 															)}
 														</div>
-														<svg
-															width="16"
-															height="16"
-															viewBox="0 0 14 14"
-															fill="none"
-															xmlns="http://www.w3.org/2000/svg"
-															className="text-[#C00000] flex-shrink-0"
-														>
-															<path
-																d="M13 1L1 13M1 1L13 13"
-																stroke="currentColor"
-																strokeWidth="2"
-																strokeLinecap="round"
-																strokeLinejoin="round"
-															/>
-														</svg>
 													</div>
 												</div>
 											)}
