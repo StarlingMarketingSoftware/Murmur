237f5dc4a799ffa6af2e4505353d50ddf4f92622
Starlingmarketing <starlingmarketingagency@gmail.com>
2025-12-25 11:41:19 -0500

    fixing the Toast popups

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -3,14 +3,26 @@ import { Toaster as SonnerToaster } from 'sonner';
 export function Toaster() {
 	return (
 		<SonnerToaster
-			richColors
 			gap={10}
 			duration={3500}
 			closeButton
 			position="bottom-right"
+			icons={{
+				success: null,
+				error: null,
+				warning: null,
+				info: null,
+				loading: null,
+			}}
 			toastOptions={{
 				duration: 3000,
-				className: '!shadow-none border-1  border-solid border-primary',
+				className: '!shadow-none',
+				style: {
+					background: '#FFFFFF',
+					border: '3px solid #000000',
+					borderRadius: '8px',
+					paddingRight: '36px',
+				},
 			}}
 		/>
 	);
