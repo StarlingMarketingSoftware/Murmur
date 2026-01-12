32d0b639f9785930d572659c948b7e119986cfa0
Starlingmarketing <starlingmarketingagency@gmail.com>
2026-01-11 19:14:46 -0500

    beginning of the drafting section

--- src/app/(public)/drafting/page.tsx
+++ src/app/(public)/drafting/page.tsx
@@ -1,9 +1,36 @@
+'use client';
+
 import React from 'react';
+import DraftingDemo from '@/components/atoms/_svg/DraftingDemo';
 
 export default function DraftingPage() {
+  React.useEffect(() => {
+    const footer = document.querySelector('footer');
+    if (footer) {
+      footer.style.display = 'none';
+    }
+    
+    return () => {
+      const footer = document.querySelector('footer');
+      if (footer) {
+        footer.style.display = '';
+      }
+    };
+  }, []);
+
   return (
     <div className="min-h-screen bg-white">
-      {/* Content will go here */}
+      <div className="w-full h-[264px] flex items-center justify-center">
+        <h1 
+          className="font-inter text-center"
+          style={{ fontSize: '65px' }}
+        >
+          Drafting
+        </h1>
+      </div>
+      <div className="flex justify-center">
+        <DraftingDemo className="translate-x-14" />
+      </div>
     </div>
   );
 }
